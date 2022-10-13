89488aa0073d79c321872f28f8cec7cfc1366c80
paoloevan <paoloev1900@gmail.com>
2022-10-13 13:50:39 +0200

    add consol log

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// creo lista della spesa
+const spesaList = [
+    'pane',
+    'latte',
+    'biscotti',
+    'tonno'
+];
+
+//stampo la lista
+let i = 0
+while (i < spesaList.length) {
+    console.log(spesaList[i]);
+
+    i++
+}
+
+//seleziono elemento DOM
