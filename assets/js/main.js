8e116f0cfe1ab7a30575fdefb1d87afc5dcaad10
paoloevan <paoloev1900@gmail.com>
2022-10-13 14:05:19 +0200

    add alternative solutions

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,12 @@ const containerEl = document.querySelector('.container ul')
 //stampo array in DOM
 let i = 0
 while (i < spesaList.length) {
-    const element = `<li>${spesaList[i]}</li>`
-    containerEl.insertAdjacentHTML('beforeend', element)
+    //const element = `<li>${spesaList[i]}</li>`
+    //containerEl.insertAdjacentHTML('beforeend', element)
+    //containerEl.innerHTML += element
+    const element = document.createElement('li')
+    element.append(spesaList[i])
+    containerEl.append(element)
 
     i++
 }
